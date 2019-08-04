27699737ec6aadd2674a776fc3ff970d3e772467
run123love <1074495962@qq.com>
2019-08-05 00:47:57 +0800

    add functionality of getting github user info

---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,30 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setClient_id("befcd4846b1b9582a04a");
+        accessTokenDTO.setClient_secret("c3992d552655e9a6bb80b1e94f8c26013d6a10af");
+        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser = githubProvider.getGithubUser(accessToken);
+        System.out.println(githubUser.getName());
+        return "index";
+    }
+}
---
+++ src/main/java/life/majiang/community/provider/GithubProvider.java
@@ -0,0 +1,51 @@
+package life.majiang.community.provider;
+
+import com.alibaba.fastjson.JSON;
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import okhttp3.*;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO accessTokenDTO) {
+        final MediaType mediaType
+                = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody body = RequestBody.create(JSON.toJSONString(accessTokenDTO), mediaType);
+        Request request = new Request.Builder()
+                .url("https://github.com/login/oauth/access_token")
+                .post(body)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String responseString = response.body().string();
+            String[] split = responseString.split("&");
+            String tokenString = split[0];
+            String token = tokenString.split("=")[1];
+            return token;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public GithubUser getGithubUser(String accessToken) {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url("https://api.github.com/user?access_token=" + accessToken)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String responseString =  response.body().string();
+            GithubUser githubUser = JSON.parseObject(responseString, GithubUser.class);
+            return githubUser;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
